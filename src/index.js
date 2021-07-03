fd71ec448d9dce1d0fbf039890834ad48b047200
Iryna Puchkova <irynapuchkova18@gmail.com>
2021-07-03 18:22:16 +0300

    config files in the project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './sass/main.scss';
+import menuItems from './menu.json';
+import menuItemsTpl from './templates/menuItemTpl.hbs'
+
+console.log(menuItems);
+console.log(menuItemsTpl({ or: 'be_happy' }));
