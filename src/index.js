ac5600130cb5c8bd22948b150aafb34cf80d0ed6
Iryna Puchkova <irynapuchkova18@gmail.com>
2021-07-05 12:17:33 +0300

    add listener and checkbox-foo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 import './sass/main.scss';
+
 import menuItems from './menu.json';
 import menuItemsTpl from './templates/menuItemTpl.hbs'
+import { on } from 'events';
 
-console.log(menuItems);
-console.log(menuItemsTpl({ or: 'be_happy' }));
+const menuItemsList = document.querySelector('.js-menu');
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const themeControlCheckbox = document.querySelector('input[type="checkbox"]');
+const body = document.querySelector('body');
+
+menuItemsList.innerHTML = menuItemsTpl(menuItems);
+
+themeControlCheckbox.addEventListener('change', checkboxHandler)
+
+savedTheme()
+
+function checkboxHandler(e) {
+
+  if (e.target.checked) {
+    body.classList.add('dark-theme')
+    localStorage.setItem('Theme', Theme.DARK)
+  } else {
+    body.classList.remove('dark-theme', 'light-theme');
+    localStorage.setItem('Theme', Theme.LIGHT)
+  }
+}
+
+function savedTheme() {
+  const savedTheme = localStorage.getItem('Theme');
+
+  if (savedTheme == Theme.DARK) {
+    themeControlCheckbox.checked = true;
+  }
+
+  body.classList.add(savedTheme);
+}
